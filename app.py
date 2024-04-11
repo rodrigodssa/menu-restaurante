ec3f51bfacfd7eabc40d788a87d627829c309ef7
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-11 06:10:56 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,10 @@
+print("sabor express\n")
+
+print(" 1. cadastrar restaurante")
+print(" 2. listar restaurante")
+print(" 3. ativar restaurante")
+print(" 4. sair\n ")
+
+opcao_escolhida = input("escolha uma opcão: ")
+print("voce escolheu", opcao_escolhida)
+
