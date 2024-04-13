50c0af91d73afb96d22d3d0dabc7b7f7ec33a363
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-13 16:29:06 -0300

    novo commit

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 print("⟆Ꭿᑲᗝᖇ ∈ⲭᕈᖇ∈⟆⟆\n")
-print(" 1. cadastrar restaurante")
-print(" 2. listar restaurante")
-print(" 3. ativar restaurante")
+print(" 1. cadastrar restaurante ")
+print(" 2. listar restaurantes ")
+print(" 3. ativar restaurante ")
 print(" 4. sair\n ")
 
-opcao_escolhida = input("escolha uma opcão: ")
-print(f"voce escolheu {opcao_escolhida} ")
+opcao_escolhida = int(input("escolha uma opcão: "))
+# opcao_escolhida = int(opcao_escolhida)
 
 
-      
 
+
+
+
+if opcao_escolhida == 1:
+    print("cadastrar restaurante")
+elif opcao_escolhida == 2: 
+    print("listar restaurantes")
+elif opcao_escolhida == 3:
+    print("ativar restaurante")
+else:
+    print("encerrar o programa")     
