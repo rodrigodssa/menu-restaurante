697026136260da59d91a4740c91bf7e804c60d51
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-04-21 18:13:29 -0300

    feat(exercio menu finalilizado)

--- ex02.py
+++ ex02.py
@@ -63,6 +63,7 @@ else:
     print('Finalizar app')
 
 # Criando funções e fazendo import
+import os
 
 def exibir_nome_do_programa():
     print("""SABOR EXPRESS
@@ -75,4 +76,30 @@ def exibir_opcoes():
     print('4. sair\n')
 
 def finalizar_app():
-    print
+    os.system('cls')
+    # os.system('clear')
+    print('Finalizando o app')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção'))
+    # opcao_escolhida = int(opcao_escolhida)
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurante')
+elif opcao_escolhida == 3:
+    print('Ativar restarurante')
+else:
+    finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+                            
+
+
