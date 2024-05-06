0bf85f31d2179b198f79ea41b58c635973904622
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-05-06 20:30:39 -0300

    feat: Adicionar exercício sobre [variaveis]

--- ex03.py
+++ ex03.py
@@ -22,3 +22,5 @@ print("Camiseta Unisex",
 departamento = input("Digite o nome do departamento: ")
 responsavel = input("Digite o nome da pessoa responsável: ")
 print("O departamento de " + departamento + " é liderado por " + responsavel + ".")
+
+# 
